e0c00aa5789957b5a7a1eae51bc32f783e2aa13f
djj19960924 <643451205@qq.com>
2020-05-19 22:55:34 +0800

    redux-thunk

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,3 +8,13 @@
 export const incrementAction = (number) => ({type:'increment',data:number});
 //累减的action
 export const decrementAction = (number) => ({type:'decrement',data:number});
+
+//异步action 换一种写法
+export const asyncIncrementAction = function(number) {
+  return dispatch => {
+    //在分发之前处理异步行为:发送ajax请求，开启定时器
+    setTimeout(()=>{
+      dispatch(incrementAction(number));
+    },2000);
+  }
+}
